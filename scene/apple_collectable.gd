b8fa71cb74c3fd72394b89faf258f861b39906c8
juan <juanignaciojara505@gmail.com>
2025-04-14 16:04:48 -0600

    Apple harvesting mechanics were added

---
+++ scene/apple_collectable.gd
@@ -0,0 +1,17 @@
+extends StaticBody2D
+
+
+
+func _ready():
+	fallfromtree()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func fallfromtree():
+	$AnimationPlayer.play("fallingfromtree")
+	await get_tree().create_timer(1.5).timeout
+	$AnimationPlayer.play("fade")
+	print("+1 apples")
+	await get_tree().create_timer(0.3).timeout
+	queue_free()
+	
