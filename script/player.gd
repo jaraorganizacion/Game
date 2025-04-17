1be76c42ae7e69e8f723a6a045bc2da399648671
juan <juanignaciojara505@gmail.com>
2025-04-16 20:45:44 -0600

    Se completo la funcionalidad de inventario

--- script/player.gd
+++ script/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 var speed = 100
 var player_state 
+@export var inv: Inv
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left","right","up","down")
@@ -40,7 +41,9 @@ func play_anim(dir):
 			
 func player():
 	pass	
-			
+
+func collect(item):
+	inv.insert(item)			
 			
 			
 			
