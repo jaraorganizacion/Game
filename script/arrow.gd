1d64b8836622c71f05157c8200a5f55fa89a7ba2
darwin3k <gondarwin123@gmail.com>
2025-04-24 15:24:46 -0600

    se agrego el sprite del enemigo

--- script/arrow.gd
+++ script/arrow.gd
@@ -12,5 +12,8 @@ func _process(delta):
 	position += (Vector2.RIGHT*speed).rotated(rotation) * delta
 
 
+func arrow_deal_damage():
+	pass
+
 func _on_visible_on_screen_enabler_2d_screen_exited():
 	queue_free()
