57ab7225bde9fb9355ad498b274f2672d0df35b4
juan <juanignaciojara505@gmail.com>
2025-04-26 22:33:24 -0600

    se termino el interior de la casa del personaje

--- script/house.gd
+++ script/house.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
 
-func _on_out_house_body_entered(body: Node2D) -> void:
+func _on_out_house_body_entered(body):
 	if body.has_method("player"):
-		get_tree().change_scene_to_file("res://scene/world.tscn")
+		# Posponemos la eliminación de nodos o el cambio de escena para evitar conflictos
+		call_deferred("_exit_house")
+		
+func _exit_house() -> void:
+	# Aquí puedes poner el código que causa el error, por ejemplo, cambiar la escena o eliminar nodos.
+	get_tree().change_scene_to_file("res://scene/world.tscn")  # Cambia a la escena exterior
--- script/world.gd
+++ script/world.gd
@@ -3,4 +3,8 @@ extends Node2D
 
 func _on_entrar_house_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
-		get_tree().change_scene_to_file("res://scene/house.tscn")
+		# Posponemos el cambio de escena para evitar conflictos con la física
+		call_deferred("_change_scene")
+		
+func _change_scene() -> void:
+	get_tree().change_scene_to_file("res://scene/house.tscn")
