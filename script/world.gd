b3ec08bdb7f50d86ad458b8d78560f6bdb5f7f4a
juan <juanignaciojara505@gmail.com>
2025-05-03 00:07:40 -0600

    se agrego un nuevo mundo

--- script/world.gd
+++ script/world.gd
@@ -8,3 +8,12 @@ func _on_entrar_house_body_entered(body: Node2D) -> void:
 		
 func _change_scene() -> void:
 	get_tree().change_scene_to_file("res://scene/house.tscn")
+
+
+#trasladarse al otro mapa
+func _on_portal_body_entered(body):
+	if body.has_method("player"):
+		call_deferred("_change_scene_world_2")
+		
+func _change_scene_world_2():
+	get_tree().change_scene_to_file("res://scene/world_2.tscn")
